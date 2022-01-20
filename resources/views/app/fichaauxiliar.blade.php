bcc861cdb5b74d4b126e8d2d05ffa4138c59469d
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-01-20 17:13:04 -0300

    Inserindo dados na View Ficha Auxiliar

--- resources/views/app/fichaauxiliar.blade.php
+++ resources/views/app/fichaauxiliar.blade.php
@@ -1,80 +1,368 @@
-<h1>Ficha auxiliar</h1>
-<ul>
-    <li>Soldo: {{ $receitas['soldo'] }}</li>
-    <li>Soldo Proporcional: {{ $receitas['soldo_prop'] }}</li>
-    <li>Compl Cota Soldo: {{ $receitas['compl_ct_soldo'] }}</li>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
 
-    <li><strong>Adicionais:</strong>
-        <ul>
-            <li>Adic Tp Sv: {{ $receitas['adic_tp_sv'] }}</li>
-            <li>Adic Disponibilidade: {{ $receitas['adic_comp_disp'] }}</li>
-            <li>Adic Habilitação: {{ $receitas['adic_hab'] }}</li>
-            <li>Adic Militar: {{ $receitas['adic_mil'] }}</li>
-            <li>Adic Férias: {{ $receitas['adic_ferias'] }}</li>
-            <li>Adic Perm: {{ $receitas['adic_perm'] }}</li>
-            <li>Adic Comp Org: {{ $receitas['adic_comp_org'] }}</li>
-            <li>Adic H Voo: {{ $receitas['hvoo'] }}</li>
-            <li>Adic PTTC: {{ $receitas['adic_pttc'] }}</li>
-            <li>Adic Natalino: {{ $receitas['adic_natalino'] }}</li>
-            <li>Acréscimo 25% soldo: {{ $receitas['acres_25_soldo'] }}</li>
-        </ul>
-    </li>
+<head>
+    <title>Ficha Auxiliar</title>
 
-    <li><strong>Gratificações:</strong>
-        <ul>
-            <li>Gratificação Loc Esp: {{ $receitas['grat_loc_esp'] }}</li>
-            <li>Gratificação Repr 2%: {{ $receitas['grat_repr_2'] }}</li>
-            <li>Gratificação de Comando: {{ $receitas['grat_repr_cmdo'] }}</li>
-        </ul>
-    </li>
+    <style>
+        td.td_calculos {
+            border: 1px solid rgb(54, 54, 54);
+            text-align: center;
+        }
 
-    <li><strong>Auxílios:</strong>
-        <ul>
-            <li>Aux Pré Escolar: {{ $receitas['aux_pre_escolar'] }}</li>
-            <li>Aux Alimentação "C": {{ $receitas['aux_alim_c'] }}</li>
-            <li>Aux Transporte: {{ $receitas['aux_transporte'] }}</li>
-            <li>Aux Natalidade: {{ $receitas['aux_natalidade'] }}</li>
-            <li>Aux Alimentação "5x": {{ $receitas['aux_alim_5x'] }}</li>
-            <li>Auxílio Invalidez: {{ $receitas['aux_invalidez'] }}</li>
-            <li>Auxílio Fardamento: {{ $receitas['aux_fard'] }}</li>
-        </ul>
-    </li>
+        td.td_assinatura {
+            border: 1px solid rgb(255, 255, 255);
+            text-align: center;
+        }
 
-    <li><strong>Outros direitos:</strong>
-        <ul>
-            <li>Salário Família: {{ $receitas['salario_familia'] }}</li>
-        </ul>
-    </li>
+        td.td_cabecalho {
+            padding: 0 .5em;
+            text-align: left
+        }
 
-    <li> * Dependente Ex-Cmbt Art. 9: {{ $receitas['dp_excmb_art_9'] }}</li>
-</ul>
+    </style>
+</head>
 
-<p>BRUTO (TOTAL): {{ $receitas['bruto_total'] }}</p>
-<p>BRUTO (IR): {{ $receitas['bruto_ir_descontos'] }}</p>
+<body>
+    <table cellpadding="0" cellspacing="0">
+        <tr>
+            <th class="td_cabecalho" style="text-align: center;" colspan="11">
+                <p>FICHA AUXILIAR</p>
+            </th>
+        </tr>
+        <tr>
+            <td class="td_cabecalho" style="text-align: left;">
+                <p>UG:</p>
+            </td>
+            <td class="td_cabecalho" colspan="3">
+                <p>35º BI</p>
+            </td>
+            <td class="td_cabecalho">
+                <p>M&Ecirc;S:</p>
+            </td>
+            <td class="td_cabecalho" colspan="6">
+                <p>SET/21</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_cabecalho">
+                <p>NOME:</p>
+            </td>
+            <td class="td_cabecalho" colspan="5">
+                <p>RAMON OLIVEIRA DOS SANTOS</p>
+            </td>
+            <td class="td_cabecalho">
+                <p>P/G:</p>
+            </td>
+            <td class="td_cabecalho">
+                <p>{{ $informacoes['pg_real_info']['pg_abrev'] }}</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_cabecalho">
+                <p>IDT:</p>
+            </td>
+            <td class="td_cabecalho" colspan="2">
+                <p>060134543-4</p>
+            </td>
+            <td class="td_cabecalho">
+                <p>CPF:</p>
+            </td>
+            <td class="td_cabecalho" colspan="9">
+                <p>000.000.000-00</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_calculos" rowspan="18">
+                <p>R</p>
+                <p>E</p>
+                <p>C</p>
+                <p>E</p>
+                <p>I</p>
+                <p>T</p>
+                <p>A</p>
+                <p>S</p>
+            </td>
+            <td class="td_calculos" colspan="2">
+                <p>DISCRIMINA&Ccedil;&Atilde;O</p>
+            </td>
+            <td class="td_calculos">
+                <p>%</p>
+            </td>
+            <td class="td_calculos">
+                <p>VALOR<br>PESQUISADO</p>
+            </td>
+            <td class="td_calculos" colspan="2">
+                <p>VALOR<br>CONTRACHEQUE</p>
+            </td>
+            <td class="td_calculos" colspan="4">
+                <p>OBSERVA&Ccedil;&Otilde;ES</p>
+            </td>
+        </tr>
 
-<h3>Descontos</h3>
-<p>P Mil 10,5%: {{ $descontos['pmil'] }}</p>
-<p>P Mil 1,5%: {{ $descontos['pmil_15'] }}</p>
-<p>P Mil 3,0%: {{ $descontos['pmil_30'] }}</p>
-<p>FuSEx 3%: {{ $descontos['fusex_3'] }}</p>
-<p>desc_dep_fusex: {{ $descontos['desc_dep_fusex'] }}</p>
-<p>pnr: {{ $descontos['pnr'] }}</p>
-<p>pens_judiciaria_1: {{ $descontos['pens_judiciaria_1'] }}</p>
-<p>pens_judiciaria_2: {{ $descontos['pens_judiciaria_2'] }}</p>
-<p>pens_judiciaria_3: {{ $descontos['pens_judiciaria_3'] }}</p>
-<p>pens_judiciaria_4: {{ $descontos['pens_judiciaria_4'] }}</p>
-<p>pens_judiciaria_5: {{ $descontos['pens_judiciaria_5'] }}</p>
-<p>pens_judiciaria_6: {{ $descontos['pens_judiciaria_6'] }}</p>
-<p>pens_judiciaria_adic_natal_1: {{ $descontos['pens_judiciaria_adic_natal_1'] }}</p>
-<p> * Adic Natalino Adiantamento: {{ $descontos['adic_natalino_valor_adiantamento'] }}</p>
-<p>pens_judiciaria_adic_natal_2: {{ $descontos['pens_judiciaria_adic_natal_2'] }}</p>
-<p>pens_judiciaria_adic_natal_3: {{ $descontos['pens_judiciaria_adic_natal_3'] }}</p>
-<p>pens_judiciaria_adic_natal_4: {{ $descontos['pens_judiciaria_adic_natal_4'] }}</p>
-<p>pens_judiciaria_adic_natal_5: {{ $descontos['pens_judiciaria_adic_natal_5'] }}</p>
-<p>pens_judiciaria_adic_natal_6: {{ $descontos['pens_judiciaria_adic_natal_6'] }}</p>
+        @isset($receitas)
+            @php
+                $contador = 0;
+            @endphp
 
-<p>imposto_renda_mensal: {{ $descontos['imposto_renda_mensal'] }}</p>
-<p>imposto_renda_adic_natal: {{ $descontos['imposto_renda_adic_natal'] }}</p>
-<p>imposto_renda_adic_ferias: {{ $descontos['imposto_renda_adic_ferias'] }}</p>
-<p>descontos_ir: {{ $descontos['descontos_ir'] }}</p>
-<p>descontos_total: {{ $descontos['descontos_total'] }}</p>
+            @foreach ($receitas as $indice => $valor)
+
+                @if ($valor['valor'] > 0 and $valor['rubrica'] != 'BRUTO TOTAL' and $valor['rubrica'] != 'BRUTO PARA IR')
+                    <tr>
+                        <td class="td_calculos" colspan="2">
+                            {{ $valor['rubrica'] }}
+                        </td>
+                        <td class="td_calculos">
+                            <!-- % AQUI -->
+                        </td>
+                        <td class="td_calculos">
+                            {{ $valor['valor'] }}
+                        </td>
+                        <td class="td_calculos" colspan="2">
+                            <span>R$ </span><input type="number" value="{{ $valor['valor'] }}" step="0.01">
+                        </td>
+                        <td class="td_calculos" colspan="4">
+                            <p>
+                                <!-- VALOR OBSERVAÇÕES AQUI -->
+                                <br> <!-- para não sumir a linha -->
+                            </p>
+                        </td>
+                    </tr>
+                    @php
+                        $contador++;
+                    @endphp
+                @endif
+
+            @endforeach
+
+            {{-- Esse for é acionado caso não tenha pelos 18 Rúbricas --}}
+            @for ($i = $contador; $i < 17; $i++)
+                <tr>
+                    <td class="td_calculos" colspan="2">
+                        <!-- RUBRICA AQUI -->
+                    </td>
+                    <td class="td_calculos">
+                        <!-- % AQUI -->
+                    </td>
+                    <td class="td_calculos">
+                        <!-- VALOR AQUI -->
+                    </td>
+                    <td class="td_calculos" colspan="2">
+                        <!-- VALOR CONTRACHQUE AQUI -->
+                    </td>
+                    <td class="td_calculos" colspan="4">
+                        <p>
+                            <!-- VALOR OBSERVAÇÕES AQUI -->
+                            <br> <!-- para não sumir a linha -->
+                        </p>
+                    </td>
+                </tr>
+            @endfor
+
+        @endisset
+
+        <!-- DESCONTOS -->
+        <tr>
+            <td class="td_calculos" rowspan="18">
+                <p>D</p>
+                <p>E</p>
+                <p>S</p>
+                <p>P</p>
+                <p>E</p>
+                <p>S</p>
+                <p>A</p>
+                <p>S</p>
+            </td>
+            <td class="td_calculos" colspan="2">
+                <p>DISCRIMINA&Ccedil;&Atilde;O</p>
+            </td>
+            <td class="td_calculos">
+                <p>%</p>
+            </td>
+            <td class="td_calculos">
+                <p>VALOR<br>PESQUISADO</p>
+            </td>
+            <td class="td_calculos" colspan="2">
+                <p>VALOR<br>CONTRACHEQUE</p>
+            </td>
+            <td class="td_calculos" colspan="4">
+                <p>OBSERVA&Ccedil;&Otilde;ES</p>
+            </td>
+            @isset($descontos)
+                @php
+                    $contador = 0;
+                @endphp
+
+                @foreach ($descontos as $indice => $valor)
+
+                    @if ($valor['valor'] > 0 and $valor['rubrica'] != 'DESCONTOS TOTAL' and $valor['rubrica'] != 'DESCONTOS PARA IR')
+            <tr>
+                <td class="td_calculos" colspan="2">
+                    {{ $valor['rubrica'] }}
+                </td>
+                <td class="td_calculos">
+                    <!-- "%" AQUI -->
+                </td>
+                <td class="td_calculos">
+                    {{ $valor['valor'] }}
+                </td>
+                <td class="td_calculos" colspan="2">
+                    <span>R$ </span><input type="number" value="{{ $valor['valor'] }}" step="0.01">
+                </td>
+                <td class="td_calculos" colspan="4">
+                    <p>
+                        <!-- VALOR OBSERVAÇÕES AQUI -->
+                        <br> <!-- para não sumir a linha -->
+                    </p>
+                </td>
+            </tr>
+            @php
+                $contador++;
+            @endphp
+            @endif
+
+            @endforeach
+
+            {{-- Esse for é acionado caso não tenha pelos 18 Rúbricas --}}
+            @for ($i = $contador; $i < 17; $i++)
+                <tr>
+                    <td class="td_calculos" colspan="2">
+                        <!-- RUBRICA AQUI -->
+                    </td>
+                    <td class="td_calculos">
+                        <!-- % AQUI -->
+                    </td>
+                    <td class="td_calculos">
+                        <!-- VALOR AQUI -->
+                    </td>
+                    <td class="td_calculos" colspan="2">
+                        <!-- VALOR CONTRACHQUE AQUI -->
+                    </td>
+                    <td class="td_calculos" colspan="4">
+                        <p>
+                            <!-- VALOR OBSERVAÇÕES AQUI -->
+                            <br> <!-- para não sumir a linha -->
+                        </p>
+                    </td>
+                </tr>
+            @endfor
+
+        @endisset
+        <tr>
+            <td class="td_calculos" colspan="4">
+                <p>SOMA</p>
+            </td>
+            <td class="td_calculos">
+                <!-- SOMA VALOR PESQUISADO AQUI -->
+            </td>
+            <td class="td_calculos" colspan="2">
+                <p>
+                    <!-- SOMA VALOR CONTRACHEQUE AQUI -->
+                </p>
+            </td>
+            <td class="td_calculos" colspan="4">
+                <p>
+                    <!-- SOMA VALOR CONTRACHEQUE AQUI -->
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_calculos" colspan="4">
+                <p>LIQUIDO A RECEBER</p>
+            </td>
+            <td class="td_calculos">
+                <p>
+                    <!-- LIQUIDO DO VALOR PESQUISADO AQUI -->
+                </p>
+            </td>
+            <td class="td_calculos" colspan="2">
+                <p>
+                    <!-- LIQUIDO DO VALOR CONTRACHEQUE AQUI -->
+                </p>
+            </td>
+            <td class="td_calculos" colspan="4">
+                <p>
+                    <!-- OBSERVAÇÕES AQUI -->
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <p>Feira de Santana/BA, 20 de janeiro de 2022.</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <p><strong>RAMON OLIVEIRA DOS SANTOS - 3º Sgt</strong></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <p>Membro da Equipe</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <p><strong>RAMON OLIVEIRA DOS SANTOS - 3º Sgt</strong></p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <p>Chefe da Equipe</p>
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <br />
+            </td>
+        </tr>
+        <tr>
+            <td class="td_assinatura" colspan="11">
+                <p>ESTA FICHA DEVER&Aacute; FICAR &Agrave; DISPOSI&Ccedil;&Atilde;O DOS &Oacute;RG&Atilde;OS DE CONTROLE
+                    INTERNO E EXTERNO, POR UM PER&Iacute;ODO NUNCA INFERIOR A UM ANO.</p>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
