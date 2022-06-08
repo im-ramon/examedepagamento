1530ce9aef25389061d5049e65c197b25fe7e66c
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-06-08 20:43:13 -0300

    fix: Remoção do usuário padrão da seed user
    Remoção do usuário padrão da seed user, evitando a exposição de dados pessoais

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -14,18 +14,18 @@ class UserSeed extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'RAMON OLIVEIRA DOS SANTOS',
-            'email' => 'ramonoliveirasantos11@gmail.com',
-            'email_verified_at' => '2022-02-26 13:02:36',
-            'password' => '$2y$10$UesylhadAKlnn6VdhiPq5eY74SUEIiiDskyketQijqIFXP8NW76Gq',
-            'post_grad' => '3º Sgt',
-            'ch_equipe_name' => 'NOME COMPLETO DO CHEFE EQUIPE',
-            'ch_equipe_pg' => '1º Ten',
-            'om' => '35º BI',
-            'local_assinatura' => 'Feira de Santana-BA',
-            'userType' => 2,
-            'remember_token' => 'FsiS7ftBaFBBxh5CtoDMtLNAvqmIMHAtnsP9FJOpui5RR4Qtwsg340tGjX7Q'
-        ]);
+        // User::create([
+        //     'name' => 'RAMON OLIVEIRA DOS SANTOS',
+        //     'email' => 'ramonoliveirasantos11@gmail.com',
+        //     'email_verified_at' => '2022-02-26 13:02:36',
+        //     'password' => '$2y$10$UesylhadAKlnn6VdhiPq5eY74SUEIiiDskyketQijqIFXP8NW76Gq',
+        //     'post_grad' => '3º Sgt',
+        //     'ch_equipe_name' => 'NOME COMPLETO DO CHEFE EQUIPE',
+        //     'ch_equipe_pg' => '1º Ten',
+        //     'om' => '35º BI',
+        //     'local_assinatura' => 'Feira de Santana-BA',
+        //     'userType' => 2,
+        //     'remember_token' => 'FsiS7ftBaFBBxh5CtoDMtLNAvqmIMHAtnsP9FJOpui5RR4Qtwsg340tGjX7Q'
+        // ]);
     }
 }
